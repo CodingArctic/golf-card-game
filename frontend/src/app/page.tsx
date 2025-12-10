1667a35fa872cdee6c06d37790858d32ea456c7f
Clyde Geyer <geyer.clyde@gmail.com>
2025-12-10 01:51:13 -0500

    fix button size & title on homepage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -34,10 +34,10 @@ export default function Home() {
         </div>
       )}
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl">Welcome to Golf Card Game Online!</h1>
+        <h1 className="text-4xl text-center">Welcome to Golf Card Game Online!</h1>
         <div className="flex gap-4 w-full items-center justify-center flex-col  sm:flex-row">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
             href="/login"
           >
             Login
