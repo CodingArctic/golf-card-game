aba9a57f4703d3bd6b10a7dfabc4f94f8c5de7ef
Clyde Geyer <geyer.clyde@gmail.com>
2025-12-21 17:31:06 -0500

    fix property names

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -52,8 +52,8 @@ export default function Home() {
             href="/instructions"
             className="text-blue-600 dark:text-blue-400 hover:underline text-sm sm:text-base flex items-center gap-1"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4">
-              <path stroke-linecap="round" stroke-linejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
+              <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
             </svg>
             How to Play
           </Link>
